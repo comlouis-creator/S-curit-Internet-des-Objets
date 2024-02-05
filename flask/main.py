d2aa288c55859168e8597322b3f6889b213a9c03
Bilal SAFI <62962573+Aquatox@users.noreply.github.com>
2024-02-04 19:40:47 -0500

    Changement de requête (sévérité basse)

--- flask/main.py
+++ flask/main.py
@@ -4,7 +4,7 @@ import requests  # pour faire des requêtes à nist
 from flask import *
 
 api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?cvssV3Severity=HIGH"  # url de l'api nist prenant une sévérité basse
-api_url2 = "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=Microsoft"
+api_url2 = "https://services.nvd.nist.gov/rest/json/cves/2.0/?lastModStartDate=2021-08-04T13:00:00.000%2B01:00&lastModEndDate=2021-10-22T13:36:00.000%2B01:00"
 params = {"param1": "valeur1", "param2": "valeur2"}
 app = Flask(__name__)
 
@@ -20,6 +20,7 @@ def home():
     # params=params
     if response.status_code == 200:
         json_data = response.json()
+        print(json_data)
         print("request successful")
         return render_template("home.html", title="Home", json_data=json_data)
     else:

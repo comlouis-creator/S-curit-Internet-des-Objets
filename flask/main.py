dbf4bce06706457859c72d02cfc8c2e6b0ea901f
bouclier91 <bouclier9191@gmail.com>
2024-02-17 18:39:42 -0500

    Ajout du filtrage des CVE dans main.py

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,7 @@
 # PROJET INTERNET DES OBJETS
 import requests  # pour faire des requêtes à nist
 import json
+import re
 from flask import *
 
 #api_url2 = "https://services.nvd.nist.gov/rest/json/cves/2.0?cvssV3Severity=HIGH"  # url de l'api nist prenant une sévérité basse
@@ -10,7 +11,16 @@ api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?"
 app = Flask(__name__)
 
 def collect_data(json_data):
-    vulnerabilities=json_data['vulnerabilities']
+    vulnerabilities=[]
+    for cve in json_data['vulnerabilities']:
+        if 'configurations' in cve['cve']:
+            for configuration in cve['cve']['configurations']:
+                if 'nodes' in configuration:
+                    for node in configuration['nodes']:
+                        if 'cpeMatch' in node:
+                            for cpe in node['cpeMatch']:
+                                if re.search("^cpe:.+:h:.+$", cpe['criteria']) != None:
+                                    vulnerabilities.append(cve)
     return vulnerabilities
 
 
